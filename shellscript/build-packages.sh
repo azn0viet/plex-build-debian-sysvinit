6c85dcf03534310ce77c745a72909a7ea948f412
Le Trong Nghia HUYNH <azn0viet@gmail.com>
2018-06-17 22:53:21 +0200

    Make download of amd64 and i386 on running shellscript

--- shellscript/build-packages.sh
+++ shellscript/build-packages.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-build_debian_package amd64_base
-# build_debian_package i386
-
 build_debian_package() {
 	DEFAULT_VERSION_IN_CONTROL_FILE="1.9.5.4339-46276db8d"
 	JSON_STRING=$(curl -s "https://plex.tv/api/downloads/1.json")
+	BASE_DIR="$1_base"
 
 	# Get the download URL (first .deb url field)
-	URL=$(echo $JSON_STRING | grep -Po '"url":.*?[^\\]"' | sed 's/"url":"//g' | sed 's/"//g' | grep '\.deb$' | head -n 1)
+	URL=$(echo $JSON_STRING | grep -Po '"url":.*?[^\\]"' | sed 's/"url":"//g' | sed 's/"//g' | grep '\.deb$' | grep $1 | head -n1)
 
 	# Set  file name (last item splitted by /)
 	FILENAME=$(echo $URL | rev | cut -d'/' -f 1 | rev)
 
 	# Set the version
-	VERSION=$(echo $FILENAME | sed 's/plexmediaserver_//g' | sed 's/_amd64.deb//g')
+	VERSION=$(echo $FILENAME | sed 's/plexmediaserver_//g' | sed "s/_$1.deb//g")
 
 	# Set the final file name
-	FINAL_FILE_NAME="plexmediaserver_$VERSION-sysvinit_amd64.deb"
+	FINAL_FILE_NAME="plexmediaserver_$VERSION-sysvinit_$1.deb"
 
 	# Set the directory where the debian package is unpacked
 	DIRECTORY=$(echo $FILENAME | sed 's/.deb//g')
 
 	# Set the tmp directory
-	TMP="tmp_plex"
+	TMP="tmp_plex_$1"
 
 	# Remove the tmp/ directory if exists
 	if [ -d $TMP ]; then
@@ -37,19 +35,19 @@ build_debian_package() {
 	dpkg -x $TMP/$FILENAME $TMP/$DIRECTORY
 
 	# Untar the "prepared folder"
-	tar xvf amd64_base.tar.xz -C $TMP
+	tar xvf $BASE_DIR.tar.xz -C $TMP
 
 	# Move usr/ directory
-	mv $TMP/$DIRECTORY/usr $TMP/amd64_base/debian
+	mv $TMP/$DIRECTORY/usr $TMP/$BASE_DIR/debian
 
 	# Change the version in the amd64_base/debian/DEBIAN/control file
-	sed -i.bak s/$DEFAULT_VERSION_IN_CONTROL_FILE/$VERSION/g $TMP/amd64_base/debian/DEBIAN/control
+	sed -i.bak s/$DEFAULT_VERSION_IN_CONTROL_FILE/$VERSION/g $TMP/$BASE_DIR/debian/DEBIAN/control
 
 	# Repack the debian package
-	dpkg-deb --build $TMP/amd64_base/debian
+	dpkg-deb --build $TMP/$BASE_DIR/debian
 
 	# Move and rename the debian.deb built
-	mv $TMP/amd64_base/debian.deb $FINAL_FILE_NAME
+	mv $TMP/$BASE_DIR/debian.deb $FINAL_FILE_NAME
 
 	# Remove the tmp/ directory
 	rm -rf $TMP
@@ -60,3 +58,5 @@ build_debian_package() {
 	echo
 }
 
+build_debian_package amd64
+build_debian_package i386
